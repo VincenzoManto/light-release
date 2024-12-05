8d8d4576604d43e0da4ab45addc2d7acca410d90
Vincenzo <vincmanto@gmail.com>
2024-12-05 08:35:12 +0100

    feat: add console logging for release notes and git commands in release script

--- bin/release.js
+++ bin/release.js
@@ -67,6 +67,8 @@ const newVersion = getNewVersion(newVersionType, config);
 
 const releaseNotes = generateReleaseNotes(newVersion, config);
 
+console.log(releaseNotes);
+
 updateVersion(newVersion, config);
 
 console.log(`New release version: ${newVersion} of type ${newVersionType.type}`);
@@ -83,6 +85,7 @@ if (config.autoCommit) {
       console.warn(`\n\x1b[33mWarning: Tag v${newVersion} already exists. Skipping tag creation.\x1b[0m\n`);
       execSync(`git add . && git commit -m "chore: release v${newVersion}" && git push`, { stdio: 'inherit' });
     } else {
+      console.log(`git add . && git commit -m "chore: release v${newVersion}" && git tag -a v${newVersion} -m "${releaseNotes.mdContent || '# Release note v' + newVersion}" && git push --follow-tags`);
       execSync(`git add . && git commit -m "chore: release v${newVersion}" && git tag -a v${newVersion} -m "${releaseNotes.mdContent || '# Release note v' + newVersion}" && git push --follow-tags`, { stdio: 'inherit' });
     }
   } catch (err) {
